cb3b6345d4324d7d83299c2d9345b64d922c28b5
unclesammy23 <samuelamrehail99@gmail.com>
2023-02-03 05:45:43 +0100

    ecomerce 1
    1

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -118,9 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFIELS_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
+STATICFIELS_DIRS = [
+    os.path.join(BASE_DIR, '/static')
+]
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+MEDIA_URL = '/images/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from .models import *
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context ={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+    context ={'items':items}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
